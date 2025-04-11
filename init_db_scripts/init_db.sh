1e9e2131c0553985f1405f3ca011d49e26a2852d
lmnguyet <lminhnguyet@lminhnguyet.tma.com.vn>
2025-04-11 17:32:11 +0700

    commit v5.5

--- init_db_scripts/init_db.sh
+++ init_db_scripts/init_db.sh
@@ -24,6 +24,18 @@ CREATE TABLE film_ratings (
     imdb_score DECIMAL(3,1),
     imdb_counts INT
 );
+CREATE TABLE genres (
+    film VARCHAR(100) NOT NULL,
+    category VARCHAR(100),
+    value VARCHAR(100)
+);
+CREATE TABLE box_office (
+    film VARCHAR(100) NOT NULL,
+    budget INT,
+    box_office_us_canada INT,
+    box_office_other INT,
+    box_office_worldwide INT
+);
 LOAD DATA INFILE '/var/lib/mysql-files/pixar_films.csv'
 INTO TABLE films
 FIELDS TERMINATED BY ','
@@ -36,6 +48,25 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '\"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+LOAD DATA INFILE '/var/lib/mysql-files/genres.csv'
+INTO TABLE genres
+FIELDS TERMINATED BY ','
+ENCLOSED BY '\"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+LOAD DATA INFILE '/var/lib/mysql-files/box_office.csv'
+INTO TABLE box_office
+FIELDS TERMINATED BY ','
+ENCLOSED BY '\"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(film, @budget, @box_office_us_canada, @box_office_other, @box_office_worldwide)
+SET 
+    budget = IF(@budget = 'NA', NULL, @budget),
+    box_office_us_canada = IF(@box_office_us_canada = 'NA', NULL, @box_office_us_canada),
+    box_office_other = IF(@box_office_other = 'NA', NULL, @box_office_other),
+    box_office_worldwide = IF(@box_office_worldwide = 'NA', NULL, @box_office_worldwide)
+;
 "
 
 echo "CSV import completed."
